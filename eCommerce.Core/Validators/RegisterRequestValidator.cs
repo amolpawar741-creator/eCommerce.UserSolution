60486f24333d1f0c44fec2be12aeb27648ff400f
Amol Pawar <amolpawar741@gmail.com>
2026-06-21 13:14:42 +0530

    gender two dots

--- eCommerce.Core/Validators/RegisterRequestValidator.cs
+++ eCommerce.Core/Validators/RegisterRequestValidator.cs
@@ -17,7 +17,7 @@ public class RegisterRequestValidator:AbstractValidator<RegisterRequest>
             .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
         RuleFor(x=>x.PersonName).NotEmpty().WithMessage("Person name cant be blank.")
             .MaximumLength(100).WithMessage("Person name must be less than 100 characters.");
-        RuleFor(x => x.Gender).NotEmpty().WithMessage("Gender is required.");
+        RuleFor(x => x.Gender).NotEmpty().WithMessage("Gender is required..");
     }
 }
 
